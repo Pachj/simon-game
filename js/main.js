a1f7f2fdb6cab4b213df976b3dde6c2409377391
Pachj <Pachj@users.noreply.github.com>
2017-04-03 15:42:28 +0200

    can random pick a new field

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@
  * Created by Henry on 03.04.17.
  */
 $(document).ready(function() {
-  $('.switch').click(function() {
-    console.log(this);
+  $('#start').click(function() {
+    startGame();
   });
 });
+
+let roundCounter = 0;
+let selectedFields = [];
+
+function startGame() {
+  generateNewField();
+}
+
+function generateNewField() {
+  const newFieldNumber = Math.floor((Math.random() * 4) + 1);
+  selectedFields.push(newFieldNumber);
+}
